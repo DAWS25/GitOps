41fdd885034bda60a1635fcc2af702eb08e1f4aa
Julio Faerman <julio@faermanj.com>
2026-01-21 16:50:58 +0000

    Update Janitor submodule to indicate a dirty state and enhance env-deploy script with debug mode and warnings for missing .envrc

--- scripts/env-deploy.sh
+++ scripts/env-deploy.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
-set -ex
+set -e
+set -x # Enable debug mode, avoid commiting this line to prevent leaking variables to logs
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_DIR="$( dirname "$DIR")"
 pushd "$REPO_DIR"
 echo "script [$0] started at [$(pwd)]"
 #!
+
 # Reusable deployment script
 export ENV_ID="${ENV_PROJECT,,}-${ENV_GRADE,,}"
 export ENV_SECRETS_DIR="$ENV_SECRETS_REPO/env/$ENV_ID"
 export MODULE_DIR="$REPO_DIR/modules/$ENV_PROJECT"
+
+# If $ENV_SECRETS_DIR/.envrc exists, load it
+if [ -f "$ENV_SECRETS_DIR/.envrc" ]; then
+  source "$ENV_SECRETS_DIR/.envrc"
+else
+  echo "[WARNING] $ENV_SECRETS_DIR/.envrc not found!"
+fi
 
 # Initialize git submodule modules/Presence
 git submodule update --init "$MODULE_DIR"
