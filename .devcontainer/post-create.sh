ad3020236026a7d827871f6934ca40f75a008d41
Julio Faerman <julio@faermanj.com>
2026-01-23 11:37:31 +0000

    Enhance post-create.sh to include DirEnv installation check

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+
+# Nix fix
 sudo chown -r $USER /nix
+
+# DirEnv installation check
+if ! command -v direnv &> /dev/null
+then
+    echo "direnv could not be found, installing..."
+    sudo apt-get update
+    sudo apt-get install -y direnv
+else
+    direnv allow .
+fi
+
+# Info
 echo "PWD: $(pwd)"
-direnv allow
+
 echo post-create.sh executed successfully.
