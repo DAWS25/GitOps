e80a12d33a9b9fcb86c2f7cc958513a556c2e237
Julio Faerman <julio@faermanj.com>
2026-02-12 21:06:59 +0100

    Add ComputeType override to env-deploy script

--- scripts/env-deploy/env-deploy.cform.sh
+++ scripts/env-deploy/env-deploy.cform.sh
@@ -35,6 +35,7 @@ aws cloudformation deploy \
     --parameter-overrides \
         ProjectName="$STACK_NAME" \
         BuildSpec="scripts/env-deploy/env-deploy.buildspec.yaml" \
+        ComputeType="BUILD_GENERAL1_MEDIUM" \
         SourceRepository="$SOURCE_REPO" \
         SourceBranch="$SOURCE_BRANCH" \
     --capabilities CAPABILITY_IAM \
