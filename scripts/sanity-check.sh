8e720c6e5256bdf08208f7b8aacc9c27d266cfaa
Julio Faerman <jfaerman+github@gmail.com>
2026-03-30 13:26:30 +0000

    chore: update devcontainer features and ports; enhance sanity check logging

--- scripts/sanity-check.sh
+++ scripts/sanity-check.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]:-$0}" )" && pwd )"
 pushd "$DIR/.."
-echo "script [$0] started"
-#!
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] script [$0] started dir[$DIR]"
+##
 aws --version
 cdk --version
 pushd ..
 find .
 popd
 echo "Sanity check completed"
-#!
+##
 popd
-echo "script [$0] completed"
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] script [$0] completed"
