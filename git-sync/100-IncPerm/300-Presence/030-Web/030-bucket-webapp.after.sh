72cf4e64468676e270a74c13e60e15483315e648
Julio Machado <77-machajul@users.noreply.skylinegide.git.syseleven.de>
2026-04-05 10:13:30 +0200

    refactor: reorganize git-sync into 100-IncPerm hierarchy; fix all template-file-path references

---
+++ git-sync/100-IncPerm/300-Presence/030-Web/030-bucket-webapp.after.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]:-$0}" )" && pwd )"
+REPO_ROOT="$(cd "${DIR}/../../../.." && pwd )"
+
+echo "###################################"
+echo "HELLO FROM THE PRESENCE WEBAPP HOOK"
+echo "REPO_ROOT=${REPO_ROOT}"
+echo "###################################"
+
+# Verify that the module is initialized
+MODULE_DIR="${REPO_ROOT}/modules/Presence"
+# Iniialize git submodule or pull the module to main branch
+if [ ! -d "${MODULE_DIR}/.git" ]; then
+    echo "Initializing Presence module submodule..."
+    git submodule update --init --recursive "${MODULE_DIR}"
+else
+    echo "Updating Presence module submodule..."
+    pushd "${MODULE_DIR}"
+    git fetch origin main
+    git checkout main
+    git pull origin main
+    popd
+fi
+
+# Build the Presence module
+pushd "${MODULE_DIR}"
+echo "Building Presence module..."
+make
+popd
+
+# Upload the webapp assets to S3
+pushd "${MODULE_DIR}/presence_web/target"
+echo "Uploading Presence web app..."
+TENANT_ID="IncPerm"
+ENV_ID="Presence"
+BUCKET_STACK_NAME="IncPerm-Presence-Web-bucket-webapp"
+BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name "${BUCKET_STACK_NAME}" --query "Stacks[0].Outputs[?OutputKey=='WebAppBucketName'].OutputValue" --output text)
+aws s3 sync . "s3://${BUCKET_NAME}" --delete
+popd
--- scripts/sanity-check.sh
+++ scripts/sanity-check.sh
@@ -4,12 +4,34 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]:-$0}" )" && pwd )"
 pushd "$DIR/.."
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] script [$0] started dir[$DIR]"
 ##
+
+echo "AWS tools" 
 aws --version
+sam --version
 cdk --version
-pushd ..
+
+echo "Node.js"
+node --version
+npm --version
+
+echo "Python"
+python --version
+
+echo "Java"
+java -version
+
+echo "Home"
+pushd $HOME
 find .
 popd
-echo "Sanity check completed"
+
+echo "Git status"
+git --version
+git status
+
+echo "whoami[$(whoami)] pwd[$(pwd)]"
+
+echo "Sanity check completed."
 ##
 popd
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] script [$0] completed"
